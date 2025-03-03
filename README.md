ff8ea6a6c072202aa38c55a209f626d25e1493f8
Abhi Mehrotra <abhimhrtr@pm.me>
2025-03-03 05:00:28 -0600

    Update .gitignore, add jest configuration, and modify README
    Signed-off-by: Abhi Mehrotra <abhimhrtr@pm.me>

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # searchpro-proxy
+
 Setting up a proxy using Cloudflare Workers to pass search queries to my SearchPro GPT.
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+// jest.config.js
+module.exports = {
+    collectCoverage: true,
+    coverageDirectory: "./coverage",
+    // other Jest configuration options...
+};
